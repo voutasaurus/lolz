f3c6b4e896e17908327ebbd0a51a38f7259e7df0
voutasaurus <voutasaurus@gmail.com>
2013-10-08 12:52:17 +1100

    Added structure to allow testing of multiple lolz
    main file calls all the lolz

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+  	reflex()
+	fmt.Println()
+	fmt.Println()
+	fmt.Println()
+  	play()
+}
--- self_reflexive.go
+++ self_reflexive.go
@@ -6,8 +6,8 @@ import (
 	"os"
 )
 
-func main() {
-	str := "package main\n\nimport (\n\t\"fmt\"\n\t\"text/template\"\n\t\"os\"\n)\n\nfunc main() {\n\tstr := {{.}}\n\ttmpl, _ := template.New(\"test\").Parse(str)\n\t_ = tmpl.Execute(os.Stdout, fmt.Sprintf(\"%q\", str))\n}"
+func reflex() {
+	str := "package main\n\nimport (\n\t\"fmt\"\n\t\"text/template\"\n\t\"os\"\n)\n\nfunc reflex() {\n\tstr := {{.}}\n\ttmpl, _ := template.New(\"test\").Parse(str)\n\t_ = tmpl.Execute(os.Stdout, fmt.Sprintf(\"%q\", str))\n}"
 	tmpl, _ := template.New("test").Parse(str)
 	_ = tmpl.Execute(os.Stdout, fmt.Sprintf("%q", str))
 }
